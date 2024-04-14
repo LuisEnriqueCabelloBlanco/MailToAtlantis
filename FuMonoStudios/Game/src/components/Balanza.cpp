7772cf96afe20976c1c2e6699d93f13896dc0d21
Alvaro <alvapina@ucm.es>
2024-04-15 01:11:44 +0200

    Paquete anim MoverTransform

--- FuMonoStudios/Game/src/components/Balanza.cpp
+++ FuMonoStudios/Game/src/components/Balanza.cpp
@@ -18,9 +18,29 @@ void Balanza::initComponent()
 
 void Balanza::initAnimations(ecs::Entity* paquete, RotarTransform* flechaRotComp)
 {
-	int cantidadPeso = paquete->getComponent<Paquete>()->getCantidadPeso();
-	std::cout << cantidadPeso;
-	flechaRotComp->setDesiredGrades(cantidadPeso);
+	if (paquete->hasComponent(ecs::cmp::PAQUETE)) {
+
+		//Obtenemos cantidad peso
+		int cantidadPeso = paquete->getComponent<Paquete>()->getCantidadPeso();
+		std::cout << cantidadPeso;
+
+		//Obtenemos Tr de balanza
+		Transform* balanzaTr = ent_->getComponent<Transform>();
+
+		//Animamos flecha
+			flechaRotComp->setDesiredGrades(cantidadPeso);
+
+		//Animamos paquete
+			MoverTransform* paqueteMovComp = paquete->getComponent<MoverTransform>();
+			Transform* paqTr = paquete->getComponent<Transform>();
+			paqueteMovComp->setEasing(Easing::EaseOutCubic);
+			paqueteMovComp->setFinalPos(Vector2D(balanzaTr->getPos().getX() + balanzaTr->getWidth() / 2 - paqTr->getWidth() / 2
+				, balanzaTr->getPos().getY() + 100));
+			paqueteMovComp->setMoveTime(1.0f);
+			paqueteMovComp->enable();
+
+		//Animamos balanza
+	}
 }
 
 void Balanza::finishAnimatios(RotarTransform* flechaRotComp)
