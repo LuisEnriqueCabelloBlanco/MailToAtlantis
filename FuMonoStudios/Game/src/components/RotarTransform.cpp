118e6ddedaebff88fb5ce302db5f6df699e26df0
VerXxuxZ <juserr04@ucm.es>
2024-04-15 00:03:18 +0200

    cojo

--- FuMonoStudios/Game/src/components/RotarTransform.cpp
+++ FuMonoStudios/Game/src/components/RotarTransform.cpp
@@ -1,1 +1,13 @@
 #include "RotarTransform.h"
+#include "Transform.h"
+
+void RotarTransform::update()
+{
+	if (myGrades_ != desiredGrades_) {
+		Vector2D pos = myTransform->getPos();
+		if (myGrades_ < desiredGrades_)
+			myTransform->setRotation(myTransform->getRotation() + animGrades_);
+		else
+			myTransform->setRotation(myTransform->getRotation() - animGrades_);
+	}
+}
--- FuMonoStudios/Game/src/components/RotarTransform.h
+++ FuMonoStudios/Game/src/components/RotarTransform.h
@@ -1,14 +1,24 @@
 #pragma once
 
 #include "../architecture/Component.h"
+
+class Transform;
+
 class RotarTransform: public ecs::Component
 {
 public:
 	__CMP_DECL__(ecs::cmp::ROTARTRANSFORM)
 
-	virtual void initComponent() {};
-	virtual void update() {};
+	virtual void initComponent();
+	virtual void update() override;
+
 private:
-	//mis
-	float mGrades_;
+	// mis grados
+	float myGrades_;
+	// grados que quiero
+	float desiredGrades_;
+	//Cantidad de grados a subir o bajar
+	float animGrades_;
+	// 
+	Transform* myTransform;
 };
